485c90d5f727bdf6044387c1bb723cddd041c908
Michelle Morin <morin2m@gmail.com>
2019-12-01 15:20:56 -0800

    add separate user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
 	return number1 + number2;
 };
@@ -14,7 +16,14 @@ var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
+// User interface (or front-end) logic:
 
-alert(divide(number1,number2));
+$(document).ready(function() {
+	$("form#add").submit(function(event) {
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1,number2);
+		$("#output").text(result);
+	});
+});
