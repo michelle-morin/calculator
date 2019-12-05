b0d1a99aa44846634c28865739b20adeabe2d243
Student <example@example.com>
2019-12-04 17:54:56 -0800

    add branch for different operators

--- js/scripts.js
+++ js/scripts.js
@@ -19,11 +19,21 @@ var divide = function(number1, number2) {
 // User interface (or front-end) logic:
 
 $(document).ready(function() {
-	$("form#add").submit(function(event) {
+	$("form#calculator").submit(function(event) {
 		event.preventDefault();
-		var number1 = parseInt($("#add1").val());
-		var number2 = parseInt($("#add2").val());
-		var result = add(number1,number2);
+		var number1 = parseInt($("#input1").val());
+		var number2 = parseInt($("#input2").val());
+		var operator = $("input:radio[name=operator]:checked").val();
+		var result;
+			if (operator === "add") {
+				result = add(number1, number2);
+			} else if (operator === "subtract") {
+				result = subtract(number1, number2);
+			} else if (operator === "multiply") {
+				result = multiply(number1, number2);
+			} else if (operator === "divide") {
+				result = divide(number1, number2);
+			}
 		$("#output").text(result);
 	});
 });
